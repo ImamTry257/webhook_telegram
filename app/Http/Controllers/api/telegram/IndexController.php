6d8a84cf18fb4e7aea0884076c7927d236d3d374
imamtry <imamtrywibowo@gmail.com>
2024-01-28 14:49:41 +0700

    init webhook telegram

---
+++ app/Http/Controllers/api/telegram/IndexController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\api\telegram;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class IndexController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $TOKEN = env('TOKEN_BOOT');
+        $api_url = "https://api.telegram.org/bot$TOKEN";
+        $update = json_decode(file_get_contents("php://input"), TRUE);
+        $chatID = $update["message"]["chat"]["id"];
+        $message = $update["message"]["text"];
+
+        if (strpos($message, "/start") === 0) {
+
+            Http::post($api_url . "/sendmessage?chat_id=" . $chatID . "&text=Haloo, test webhooks dicoffeean.com.&parse_mode=HTML");
+        }
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'OK',
+            'code'      => 200
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

65a344a3851c9b7461fea1c2fdb815d7fd755938
imamtry <imamtrywibowo@gmail.com>
2024-01-28 15:22:06 +0700

    fixing route

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/webhook/telegram', [IndexController::class, 'index']);
+Route::post('/telegram', [IndexController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
